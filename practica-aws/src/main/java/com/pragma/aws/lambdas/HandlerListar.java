3dce710d08d0493158c208435da1a47dfe4f041d
Juan Fuentes <43227783+Juanandresfl@users.noreply.github.com>
2021-07-23 16:10:04 -0500

    lambdas aws

--- practica-aws/src/main/java/com/pragma/aws/lambdas/HandlerListar.java
+++ practica-aws/src/main/java/com/pragma/aws/lambdas/HandlerListar.java
@@ -1,4 +1,4 @@
-package com.pragma.aws;
+package com.pragma.aws.lambdas;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
@@ -6,11 +6,12 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.pragma.aws.Cliente;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Handler implements RequestHandler<Cliente,Object> {
+public class HandlerListar implements RequestHandler<Cliente,Object> {
     @Override
     public Object handleRequest(Cliente cliente, Context context) {
         AmazonDynamoDB db = AmazonDynamoDBClientBuilder.defaultClient();
---
+++ practica-aws/src/main/java/com/pragma/aws/lambdas/HandlerRegistrar.java
@@ -0,0 +1,22 @@
+package com.pragma.aws.lambdas;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.pragma.aws.Cliente;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HandlerRegistrar implements RequestHandler<Cliente,Object> {
+    @Override
+    public Object handleRequest(Cliente cliente, Context context) {
+        AmazonDynamoDB db = AmazonDynamoDBClientBuilder.defaultClient();
+        DynamoDBMapper mapper = new DynamoDBMapper(db);
+        mapper.save(cliente);
+        return cliente;
+    }
+}
